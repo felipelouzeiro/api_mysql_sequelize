fa3e4dc56f36866437f488d410c5324c643a0db0
felipe <fl.louzeiro@gmail.com>
2022-03-21 19:23:03 -0300

    feat: adiciona inicializaçao do sequelize no app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+const db = require("./database/models");
+db.sequelize.sync();
 
 app.get('/', (_req, res) => {
   res.send("Funcionando")
