fac240043f6fb4ed4f998f7eec49963de1ff5ab9
felipe <fl.louzeiro@gmail.com>
2022-03-21 20:46:10 -0300

    feat: cria biblioteca de status http

--- src/database/controllers/users.js
+++ src/database/controllers/users.js
@@ -1,17 +1,18 @@
 const { genToken } = require('../../utils/JWT');
 const createUser = require('../../service/Users/createUser');
+const { Created } = require('../../utils/status-http-library');
 
 module.exports = async (req, res, next) => {
   try {
     const { displayName, email, password } = req.body;
 
-    const response = await createUser( displayName, email, password );
+    const response = await createUser({ displayName, email, password });
 
     const { password: pass, ...withoutPassword } = response; // removo o password antes de retornar as infomações;
 
     const codeToken = await genToken(withoutPassword);
 
-    res.status(201).json({ token: codeToken });
+    res.status(Created).json({ token: codeToken });
   } catch (error) {
     next(error);
   }
---
+++ src/utils/status-http-library.js
@@ -0,0 +1,10 @@
+module.exports = {
+  OK: 200,
+  Created: 201,
+  NoContent: 204,
+  BadRequest: 400,
+  Unauthorized: 401,
+  NotFound: 404,
+  Conflit: 409,
+  InternalError: 500,
+};
