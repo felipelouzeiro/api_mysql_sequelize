af61edcf5b33be454e77827d4c92d6b0915825f4
felipe <fl.louzeiro@gmail.com>
2022-03-21 22:54:01 -0300

    feat: personaliza mensagens de erro no schema yup

---
+++ src/utils/Schemas/schemas.js
@@ -0,0 +1,17 @@
+const yup = require('yup');
+
+const userSchema = yup.object({
+  displayName: yup.string()
+  .min(8, 'DisplayName must be 8 or more characters.')
+  .required('DisplayName is required.'),
+  email: yup.string()
+  .email('Invalid e-mail.')
+  .required('E-mail is required.'),
+  password: yup.string()
+  .min(6, 'Password must be 6 or more characters.')
+  .required('Password is required.')
+});
+
+module.exports = {
+  userSchema,
+}
