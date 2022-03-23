0aab710b10588735da04b7c0ee9bac2557f8d76e
felipe <fl.louzeiro@gmail.com>
2022-03-22 22:17:50 -0300

    feat: adiciona verificaçao para as categoryIds antes de criar o post

--- src/database/services/blogPost.js
+++ src/database/services/blogPost.js
@@ -1,4 +1,5 @@
-const { BlogPost } = require('../models');
+const { Op } = require('sequelize'); // https://sequelize.org/v5/manual/querying.html
+const { BlogPost, Category } = require('../models');
 const { postSchema } = require('../../utils/Schemas/schemas');
 const handlingError = require("../../utils/Helpers/handlingError");
 const { BadRequest } = require('../../utils/Helpers/status-http-library');
@@ -8,6 +9,15 @@ const create = async ({ title, content, categoryIds, userId }) => {
     await postSchema.validate({ title, content, categoryIds })
   } catch (error) {
     if (error) { throw handlingError(BadRequest, error.errors[0]); }
+  }
+
+  // verifica se as categorias informadas existem no db
+  const existingCategories = await Category.findAll({
+    where: { id: { [Op.in]: categoryIds } },
+  });
+
+  if (existingCategories.length !== categoryIds.length) {
+    throw handlingError(BadRequest, '"categoryIds" not found');
   }
 
   const response = await BlogPost.create({ title, content, categoryIds, userId });
